ba86578ce7b88523920bac47b8e447ed3a3b94bf
DaryaBuxmanova <buxmanova@mail.ru>
2017-12-27 22:17:47 +0300

    add class enemy:entity

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream> 
 #include <sstream>
 #include <SFML/Graphics.hpp>
@@ -162,6 +161,99 @@ void update(float time) //функци€ "оживлени€/обновлени€" объекта класса. ѕринима
 };
 
 
+//////////////////////////// Ћј—— ¬–ј√ј////////////////////////
+class Enemy :public Entity {
+public:
+	int direction;//направление движени€ врага
+	Enemy(Image &image, float X, float Y, int W, int H, std::string Name) :Entity(image, X, Y, W, H, Name) {
+		if (name == "Enemy1") {
+			//«адаем спрайту один пр€моугольник дл€
+			//вывода одного игрока. IntRect Ц дл€ приведени€ типов
+			sprite.setTextureRect(IntRect(0, 0, w, h));
+			direction = rand() % (3); //Ќаправление движени€ врага задаЄм случайным образом
+									  //через генератор случайных чисел
+			speed = 0.1;//даем скорость.этот объект всегда двигаетс€
+			dx = speed;
+		}
+	}
+
+	void checkCollisionWithMap(float Dx, float Dy)//ф-ци€ проверки столкновений с картой
+	{
+		for (int i = y / 37; i < (y + h) / 37; i++)//проходимс€ по элементам карты
+			for (int j = x / 37; j<(x + w) / 37; j++)
+			{
+				if (TileMap[i][j] == '0')//если элемент - тайлик земли
+				{
+					if (Dy > 0) {
+						y = i * 37 - h;  dy = -0.1;
+						direction = rand() % (4); //Ќаправление движени€ врага
+					}//по Y 
+					if (Dy < 0) {
+						y = i * 37 + 37; dy = 0.1;
+						direction = rand() % (4);//Ќаправление движени€ врага 
+					}//столкновение с верхними кра€ми 
+					if (Dx > 0) {
+						x = j * 37 - w; dx = -0.1;
+						direction = rand() % (4);//Ќаправление движени€ врага 
+					}//с правым краем карты
+					if (Dx < 0) {
+						x = j * 37 + 37; dx = 0.1;
+						direction = rand() % (4); //Ќаправление движени€ врага
+					}// с левым краем карты
+				}
+			}
+	}
+
+	void update(float time)
+	{
+		if (name == "Enemy1") {//дл€ персонажа с таким именем логика будет такой
+
+			if (life) {//провер€ем, жив ли герой
+				switch (direction)//делаютс€ различные действи€ в зависимости от состо€ни€
+				{
+				case 0: {//состо€ние идти вправо
+					dx = speed;
+					CurrentFrame += 0.005*time;
+					if (CurrentFrame > 2) CurrentFrame -= 2;
+					sprite.setTextureRect(IntRect(0 , 0, 35, 35));
+					break;
+				}
+				case 1: {//состо€ние идти влево
+					dx = -speed;
+					CurrentFrame += 0.005*time;
+					if (CurrentFrame > 2) CurrentFrame -= 2;
+					sprite.setTextureRect(IntRect(33 , 0, 35, 35));
+					break;
+				}
+				case 2: {//идти вверх
+					dy = -speed;
+					CurrentFrame += 0.005*time;
+					if (CurrentFrame > 2) CurrentFrame -= 2;
+					sprite.setTextureRect(IntRect(65, 0, 35, 35));
+					break;
+				}
+				case 3: {//идти вниз
+					dy = speed;
+					CurrentFrame += 0.005*time;
+					if (CurrentFrame > 2) CurrentFrame -= 2;
+					sprite.setTextureRect(IntRect(100, 0, 35, 35));
+					break;
+				}
+				}
+
+				x += dx*time; //движение по УXФ
+				checkCollisionWithMap(dx, 0);//обрабатываем столкновение по ’
+
+				y += dy*time; //движение по УYФ
+				checkCollisionWithMap(0, dy);//обрабатываем столкновение по Y
+
+				sprite.setPosition(x, y); //спрайт в позиции (x, y).
+
+				if (health <= 0) { life = false; }//если жизней меньше 0, либо равно 0, то умираем
+			}
+		}
+	}
+};//класс Enemy закрыт
 
 
 
@@ -193,8 +285,12 @@ int main()
 
 	Image heroImage;
 	heroImage.loadFromFile("images/pacman.png");// загрузка изображени€ игрока
+	Image EnemyImage;
+	EnemyImage.loadFromFile("images/enemypac.png"); // загружаем изображение врага
+
 
 	Player p(heroImage, 250, 250, 35, 35, "Pacman");
+	Enemy Enemy(EnemyImage, 250, 200, 35, 35, "Enemy1");//объект класса врага
 
 	int createObjectForMapTimer = 0;//ѕеременна€ под врем€ дл€ генерировани€ камней
 
@@ -225,6 +321,7 @@ int main()
 
 		p.update(time); //оживл€ем объект УpФ класса УPlayerФ с помощью времени sfml,
 						// передава€ врем€ в качестве параметра функции update. 
+		Enemy.update(time);//оживл€ем объект врага
 		window.clear();// чистим экран 
 	
 	/////////////////////////////–исуем карту/////////////////////
@@ -265,6 +362,7 @@ int main()
 
 
 	window.draw(p.sprite);//рисуем спрайт объекта УpФ класса УPlayerФ
+	window.draw(Enemy.sprite);//рисуем спрайт объекта УpФ класса УPlayerФ
 	window.display();
 }
 	return 0;
